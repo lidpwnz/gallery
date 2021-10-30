c73fdbec9bd394b24f10beeac9df1623af9ea360
davidlee <lidpwnz@gmail.com>
2021-10-30 11:00:58 +0600

    Init commit

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,15 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ]
+}
+
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
