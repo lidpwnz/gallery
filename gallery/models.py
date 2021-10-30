ffeb32dd30cbf0747b6697c14ac58d2ef99463d1
davidlee <lidpwnz@gmail.com>
2021-10-30 11:19:24 +0600

    #1 Описал модель фото

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    img = models.ImageField(upload_to='avatars')
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='photos')
+    users_in_favourites = models.ManyToManyField(get_user_model(), db_table='favourites', related_name='favourites')
